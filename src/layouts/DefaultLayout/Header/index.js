7da5c546bed2848d559b7eda37446e6f0fe687f3
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2022-09-20 21:54:12 +0700

    Build UI: Header part 1 - Logo, Search bar

--- src/layouts/DefaultLayout/Header/index.js
+++ src/layouts/DefaultLayout/Header/index.js
@@ -1,12 +1,36 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import styles from './Header.module.scss';
+import logo from '~/assets/images/logo.png';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function Header() {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('inner')}>Test</div>
+      <div className={cx('inner')}>
+        <div className={cx('wrapper-logo')}>
+          <img className={cx('logo')} src={logo} alt="Logo"></img>
+          <div className={cx('logo-title')}>BookStore</div>
+        </div>
+
+        <div className={cx('search')}>
+          <input placeholder="Search your books" spellCheck={false}></input>
+          <button className={cx('clear-btn')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <div className={cx('loading', 'loading--hidden')}>
+            <FontAwesomeIcon icon={faSpinner} />
+          </div>
+          <button className={cx('search-btn')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+          </button>
+        </div>
+
+        <div className={cx('actions')}>Testshfjfwehkerwkrrhhrk</div>
+      </div>
     </header>
   );
 }
